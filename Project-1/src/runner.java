1c99042c1e00cc3f81f48e1f6588fa50a5948789
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-10-12 09:24:31 -0400

    asdf

--- Project-1/src/runner.java
+++ Project-1/src/runner.java
@@ -1,4 +1,11 @@
 
 public class runner {
-	public static void main
+	public static void main(String [] args) {
+	//	KKimLib.dateStr(10/12/2017);
+	//	KKimLib.isFibonnaci(13);
+		
+	//	DLiLib.isPalindrome();
+	//	DLiLib.sumUpTo();
+	//  DLiLib.quadraticSolver();
+	}
 }
