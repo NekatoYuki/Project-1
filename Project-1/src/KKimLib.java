16548134204ad64ca512b35e6111e1a4987a10fc
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-10-12 09:19:45 -0400

    project 1

---
+++ Project-1/src/KKimLib.java
@@ -0,0 +1,34 @@
+
+public class KKimLib {
+
+//tinyurl.com/Levinmethodmadness
+	
+//dateStr
+	
+	public static String dateStr(String str)
+	{
+		System.out.println(str.substring(3,5) + " - " + str.substring(0,2) + " - " + str.substring(6));
+	}
+
+//isFibonnaci
+	
+	public static boolean isFibonnaci(int num) {
+		if (num<0) return false;
+		else
+		{
+			int x=0;
+			int y=1;
+			int z=0;
+			while (z<num)
+				{
+					z=x+y;
+					x=y;
+					y=z;
+				}
+			if (num==z) return true;
+			else return false;
+		}
+	}
+}
+
+// 
