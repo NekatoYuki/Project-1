de218f94e488f6de3389b658b5bbff2d108be09a
Kenneth Kim <Kenneth Kim@LAPTOP-30703M7U.fios-router.home>
2017-10-14 17:27:33 -0400

    a

--- Project-1/src/runner.java
+++ Project-1/src/runner.java
@@ -3,9 +3,9 @@ public class runner {
 	public static void main(String [] args) {
 		KKimLib.dateStr("10/12/2017");
 		KKimLib.isFibonnaci(13);
-	  KKimLib.multiplicationTable(4,7);
+		KKimLib.multiplicationTable(4,7);
 	//	DLiLib.isPalindrome();
 	//	DLiLib.sumUpTo();
-	//  DLiLib.quadraticSolver();
+	//	DLiLib.quadraticSolver();
 	}
 }
