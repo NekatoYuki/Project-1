030639895620ebbd44e52b1b78be63feb6b4afbe
pjdan <pjdan@DESKTOP-5CQOPLM.home>
2017-10-24 00:36:58 -0400

    REALLY FINISHED

--- Project-1/src/runner.java
+++ Project-1/src/runner.java
@@ -4,8 +4,8 @@ public class runner {
 		//KKimLib.dateStr("10/12/2017");
 		//KKimLib.isFibonnaci(13);
 		//KKimLib.multiplicationTable(4,7);
-		DLiLib.isPalindrome("string");
 		DLiLib.sumUpTo(10);
 		DLiLib.quadraticSolver(1,-4,4);
+		DLiLib.isPalindrome("string");
 	}
 }
